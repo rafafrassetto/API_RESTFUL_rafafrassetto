9b153a4eaf0323e05b9e548e9675528b2eebef2a
rafafrassetto <rafafrass@gmail.com>
2024-09-29 00:14:10 -0300

    API_RESTFUL_rafafrassetto

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 // Arquivo para inicializar o servidor e definir as rotas para requisições GET e POST
 const express = require('express');
 const mealController = require('./controllers/mealController');
-const path = require('path'); // Importar o módulo path
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
-// Rota de consulta às refeições
 app.get('/meals', mealController.getMeals);
-
-// Rota para criar uma nova refeição
 app.post('/meals', mealController.createMeal);
 
-// Rota para a página /sobre
 app.get('/sobre', (req, res) => {
   res.json({
     estudante: "Rafael Frassetto",
@@ -27,7 +23,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
--- src/controllers/mealController.js
+++ src/controllers/mealController.js
@@ -4,16 +4,12 @@ const mealService = require('../services/mealService');
 // Array para armazenar as refeições
 let meals = [];
 
-// Controlador que lida com a requisição GET para '/meals'
 exports.getMeals = async (req, res) => {
   try {
-    // Buscando refeições da API externa
     const externalMeals = await mealService.fetchMeals();
     
-    // Combina as refeições externas com as armazenadas
     const allMeals = [...externalMeals, ...meals];
 
-    // Retorna todas as refeições
     res.json(allMeals);
   } catch (error) {
     res.status(500).json({ error: 'Erro ao buscar as refeições' });
@@ -22,12 +18,9 @@ exports.getMeals = async (req, res) => {
 
 // Controlador que lida com a requisição POST para '/meals'
 exports.createMeal = (req, res) => {
-  // Obtém a nova refeição do corpo da requisição
   const newMeal = req.body;
 
-  // Armazena a nova refeição no array
   meals.push(newMeal);
   
-  // Retorna a refeição recebida
   res.status(201).json({ message: 'Refeição recebida com sucesso', meal: newMeal });
 };
